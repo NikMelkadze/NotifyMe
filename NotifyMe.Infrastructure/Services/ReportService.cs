858f92d60ed9bb0e94c903ce017b19ee6e1aea5b
Nikoloz <nikamelqadze26@Gmail.com>
2025-10-10 14:58:51 +0400

    add get reporting by company id

--- NotifyMe.Infrastructure/Services/ReportService.cs
+++ NotifyMe.Infrastructure/Services/ReportService.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using NotifyMe.Application.Contracts;
 using NotifyMe.Application.Models;
+using NotifyMe.Domain.Enums;
 using NotifyMe.Persistence;
 
 namespace NotifyMe.Infrastructure.Services;
@@ -11,6 +12,21 @@ public class ReportService(ApplicationDbContext dbContext) : IReportService
     {
         return await dbContext.UserSavedProducts
             .Where(r => r.IsActive)
+            .GroupBy(r => new { r.Name, r.Shop })
+            .Select(g => new SavedProduct
+            {
+                Name = g.Key.Name,
+                Shop = g.Key.Shop.ToString(),
+                Count = g.Count()
+            }).Take(top)
+            .OrderByDescending(x => x.Count)
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<IEnumerable<SavedProduct>> GetCompanyTopSavedProducts(int top, Shop shop, CancellationToken cancellationToken)
+    {
+        return await dbContext.UserSavedProducts
+            .Where(r => r.Shop==shop && r.IsActive )
             .GroupBy(r => new { r.Name, r.Shop })
             .Select(g => new SavedProduct
             {
