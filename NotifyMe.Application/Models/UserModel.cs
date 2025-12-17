2b87df9b983e13e2bea6f2d0e563067a8e303e38
Nikoloz <nikamelqadze26@Gmail.com>
2025-12-17 12:48:16 +0400

    update User model validation

--- NotifyMe.Application/Models/UserModel.cs
+++ NotifyMe.Application/Models/UserModel.cs
@@ -5,10 +5,14 @@ namespace NotifyMe.Application.Models;
 public record UserModel(
     [Required, EmailAddress] string Email,
     [StringLength(20, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 20 characters.")]
+    [RegularExpression(
+        @"^(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).+$",
+        ErrorMessage = "Password must contain at least one uppercase letter and one special character.")]
     [Required]
     string Password,
     [Required] string FirstName,
     [Required] string LastName,
-    [Required, Phone, StringLength(9)] string PhoneNumber);
+    [Required, Phone, StringLength(9, MinimumLength = 9, ErrorMessage = "Phone number must be exactly 9 digits.")]
+    string PhoneNumber);
 
 public record LoginModel(string EmailOrPhoneNumber, string Password);
