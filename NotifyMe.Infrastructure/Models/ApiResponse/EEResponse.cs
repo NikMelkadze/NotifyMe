b811a880c179926cb1e45a93673383d9c85f8822
Nikoloz <nikamelqadze26@Gmail.com>
2026-04-05 17:55:36 +0400

    add ee response

--- NotifyMe.Infrastructure/Models/ApiResponse/EEResponse.cs
+++ NotifyMe.Infrastructure/Models/ApiResponse/EEResponse.cs
@@ -1,3 +1,13 @@
 namespace NotifyMe.Infrastructure.Models.ApiResponse;
 
-public class EEResponse : ProductBase;
+public class EeResponse
+{
+    public EeResponseProduct Product { get; set; } = null!;
+}
+
+public class EeResponseProduct
+{
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; }
+    public decimal PreviousPrice { get; set; }
+}
